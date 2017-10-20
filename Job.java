0352413fe32e43bd08a381afafce015dff3e59e0
tzporthouse <32957735+tzporthouse@users.noreply.github.com>
2017-10-20 14:12:33 +0100

    Update and rename Job to Job.java

--- Job.java
+++ Job.java
@@ -5,12 +5,12 @@ public class Job implements Runnable {
     this.jobNumber = jobNumber;
     }
   public void run() {
-    System.out.println("Job: " + jobNumber + "is being processed by thread : " + Thread.currentThread().getName();
+    System.out.println("Job: " + jobNumber + "is being processed by thread : " + Thread.currentThread().getName());
     try{
       Thread.sleep((int)(1000));
     } catch (InterruptedException e) {}
     
-    System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName();
+    System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName());
   } 
   
   }
